9c4d6f604dcc33fe497af0960c43b551e5ea2740
a2kad <reshetnikovruslan705@gmail.com>
2023-09-14 17:48:28 +0200

    10 questions created

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -7,6 +7,14 @@ import SignUpScreen from '../screens/SignUpScreen';
 import SignUpNameScreen from '../screens/SignUpNameScreen';
 import Question1Screen from '../screens/questions/Question1Screen';
 import Question2Screen from '../screens/questions/Question2Screen';
+import Question3Screen from '../screens/questions/Question3Screen';
+import Question4Screen from '../screens/questions/Question4Screen';
+import Question5Screen from '../screens/questions/Question5Screen';
+import Question6Screen from '../screens/questions/Question6Screen';
+import Question7Screen from '../screens/questions/Question7Screen';
+import Question8Screen from '../screens/questions/Question8Screen';
+import Question9Screen from '../screens/questions/Question9Screen';
+import Question10Screen from '../screens/questions/Question10Screen';
 import useAuth from '../hooks/useAuth';
 import HomeScreen from '../screens/HomeScreen';
 
@@ -22,6 +30,15 @@ export default function AppNavigation() {
                     <Stack.Screen name="SignUpName" options={{headerShown: false}} component={SignUpNameScreen} />
                     <Stack.Screen name="Question1" options={{headerShown: false}} component={Question1Screen} />
                     <Stack.Screen name="Question2" options={{headerShown: false}} component={Question2Screen} />
+                    <Stack.Screen name="Question3" options={{headerShown: false}} component={Question3Screen} />
+                    <Stack.Screen name="Question4" options={{headerShown: false}} component={Question4Screen} />
+                    <Stack.Screen name="Question5" options={{headerShown: false}} component={Question5Screen} />
+                    <Stack.Screen name="Question6" options={{headerShown: false}} component={Question6Screen} />
+                    <Stack.Screen name="Question7" options={{headerShown: false}} component={Question7Screen} />
+                    <Stack.Screen name="Question8" options={{headerShown: false}} component={Question8Screen} />
+                    <Stack.Screen name="Question9" options={{headerShown: false}} component={Question9Screen} />
+                    <Stack.Screen name="Question10" options={{headerShown: false}} component={Question10Screen} />
+                    
                 </Stack.Navigator>
             </NavigationContainer>
         )
---
+++ src/screens/questions/Question8Screen.js
@@ -0,0 +1,43 @@
+import { View, Text, SafeAreaView, TouchableOpacity, FlatList } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+
+export default function Question1Screen() {
+    const navigation = useNavigation();
+    return (
+        <SafeAreaView className='flex-1'>
+            <View className='flex-1 justify-top items-center m-6'>
+                <View className='py-6'>
+                    <Text className='font-bold text-4xl leading-10 text-gray-700 text-center py-4'>Question 8/10</Text>
+                </View>
+                <View className='flex justify-center mx-2'>
+                    <Text className='text-justify text-xl text-gray-500'>Avez-vous des connaissances concernant les logiciels et systèmes d'exploitation open source et autres ?  </Text>
+                    <FlatList
+                        data={[
+                            { key: 'LibreOffice' },
+                            { key: 'Ubuntu' },
+                            { key: 'Framasoft' },
+                            { key: 'Word' },
+                            { key: 'Excel' },
+                        ]}
+                        renderItem={({ item }) => <Text className='text-justify text-xl text-gray-500 p-2'>{`\u2022 ${item.key}`}</Text>}
+                    />
+                </View>
+            </View>
+            <View className='flex-row justify-around mb-11'>
+
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('Question9')}
+                    className="py-3 px-7 bg-yellow-400 mx-7 rounded-xl">
+                    <Text className="text-xl font-bold text-center text-gray-700">Oui</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('Question9')}
+                    className="py-3 px-7 bg-yellow-400 mx-7 rounded-xl">
+                    <Text className="text-xl font-bold text-center text-gray-700">Non</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    )
+}
