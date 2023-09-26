897617aeb0ca1e03b771d196e3fb59d2f932c93d
a2kad <reshetnikovruslan705@gmail.com>
2023-09-26 17:50:59 +0200

    Phone number style

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -15,22 +15,27 @@ const SignUpScreen = () => {
     const [valid, setValid] = useState(false);
     const phoneInput = useRef(null);
     const storeData = async () => {
-        
-        try {
-            const checkValid = phoneInput.current?.isValidNumber(value);
-            console.log('value ',value);
-            setValid(checkValid ? checkValid : false);
-            console.log('Valid phone number: ',valid);
-            await AsyncStorage.setItem('phoneNumber', phoneNumber);
-            await AsyncStorage.setItem('userEmail', email);
-            await AsyncStorage.setItem('userPass', password);
-            
-            console.log('phone  : ', phoneNumber);
-            console.log('email  : ', email);
-            console.log('pass  : ', password);
-            navigation.navigate('Question1')
-        } catch (e) {
-            console.log('Async error : ', e.message)
+        const checkValid = phoneInput.current?.isValidNumber(value);
+        console.log('value ', value);
+        setValid(checkValid ? checkValid : false);
+        console.log('Valid phone number: ', valid);
+        if (checkValid === true) {
+            try {
+
+                await AsyncStorage.setItem('phoneNumber', phoneNumber);
+                await AsyncStorage.setItem('userEmail', email);
+                await AsyncStorage.setItem('userPass', password);
+
+                console.log('phone  : ', phoneNumber);
+                console.log('email  : ', email);
+                console.log('pass  : ', password);
+                navigation.navigate('Question1')
+            } catch (e) {
+                console.log('Async error : ', e.message)
+            }
+        }
+        else {
+            console.log("Telephone isn't valid")
         }
     };
 
@@ -51,15 +56,22 @@ const SignUpScreen = () => {
                 <View className='flex justify-center mx-4'>
                     <Text className='text-2xl text-gray-500'>Renseigner votre e-mail et mot de passe pour continuer</Text>
                 </View>
-                <View className='mx-4 '>
+                <View className='mx-5 '>
                     {/* <Text className='text-gray-700 my-2 p-2'>Numéro de téléphone</Text>
                 <TextInput className='p-4 bg-gray-200 text-gray-700 rounded-2xl' placeholder='Téléphone'></TextInput> */}
+                <Text className='text-gray-700 my-2 p-2'>Numéro de téléphone</Text>
                     <PhoneInput
                         ref={phoneInput}
                         defaultValue={phoneNumber}
                         defaultCode='FR'
                         onChangeText={(text) => { setValue(text) }}
-                        onChangeFormattedText={(text) =>{ setPhoneNumber(text)}} />
+                        onChangeFormattedText={(text) => { setPhoneNumber(text) }}
+                        codeTextStyle={{color:'#374151'}}
+                        containerStyle={{borderRadius:14}}
+                        textContainerStyle={{backgroundColor:'#e5e7eb', borderRadius:14 }}
+                        textInputStyle={{fontSize:14}}
+                        flagButtonStyle={{}}
+                        placeholder='Numéro de téléphone' />
 
                     <Text className='text-gray-700 my-2 p-2'>E-mail</Text>
                     <TextInput className='p-4 bg-gray-200 text-gray-700 rounded-2xl' placeholder='E-mail'
