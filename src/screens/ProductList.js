f6a70ea698fc2f5541c1ec48e65e8aae01b2615c
a2kad <reshetnikovruslan705@gmail.com>
2023-10-13 16:16:26 +0200

    Linking bug fixed

--- src/screens/ProductList.js
+++ src/screens/ProductList.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, Linking } from 'react-native'
 import React from 'react'
 
 export default function ProductList() {
