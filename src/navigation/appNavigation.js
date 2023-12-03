770e4f12d8530568a3dc53fd8f697abdb5b1f99a
a2kad <reshetnikovruslan705@gmail.com>
2023-12-03 18:36:53 +0100

    Calculation correct

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -74,21 +74,21 @@ export default function AppNavigation() {
                         <Stack.Screen name='Login' options={{ headerShown: false }} component={LoginScreen} />
                         <Stack.Screen name="SignUp" options={{ headerShown: false }} component={SignUpScreen} />
                         <Stack.Screen name="SignUpName" options={{ headerShown: false }} component={SignUpNameScreen} />
-                        <Stack.Screen name="Question1" options={{ headerShown: false }} component={Question1Screen} />
                         <Stack.Screen name="Politique" options={{ headerShown: true, title: 'Politique de Confidentialité' }} component={PolitiqueScreen} />
-                        <Stack.Screen name="Question2" options={{ headerShown: false, gestureEnabled: false }} component={Question2Screen} />
-                        <Stack.Screen name="Question3" options={{ headerShown: false, gestureEnabled: false }} component={Question3Screen} />
-                        <Stack.Screen name="Question4" options={{ headerShown: false, gestureEnabled: false }} component={Question4Screen} />
-                        <Stack.Screen name="Question5" options={{ headerShown: false, gestureEnabled: false }} component={Question5Screen} />
-                        <Stack.Screen name="Question6" options={{ headerShown: false, gestureEnabled: false }} component={Question6Screen} />
-                        <Stack.Screen name="Question7" options={{ headerShown: false, gestureEnabled: false }} component={Question7Screen} />
-                        <Stack.Screen name="Question8" options={{ headerShown: false, gestureEnabled: false }} component={Question8Screen} />
-                        <Stack.Screen name="Question9" options={{ headerShown: false, gestureEnabled: false }} component={Question9Screen} />
-                        <Stack.Screen name="Question10" options={{ headerShown: false, gestureEnabled: false }} component={Question10Screen} />
-                        <Stack.Screen name="Question11" options={{ headerShown: false, gestureEnabled: false }} component={Question11Screen} />
-                        <Stack.Screen name="Question12" options={{ headerShown: false, gestureEnabled: false }} component={Question12Screen} />
-                        <Stack.Screen name="ResultatFormation" options={{ headerShown: false, gestureEnabled: false }} component={ResultatFormation} />
-                        <Stack.Screen name="ResultatProduits" options={{ headerShown: false, gestureEnabled: false }} component={ResultatProduits} />
+                        <Stack.Screen name="Question1" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question1Screen} />
+                        <Stack.Screen name="Question2" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question2Screen} />
+                        <Stack.Screen name="Question3" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question3Screen} />
+                        <Stack.Screen name="Question4" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question4Screen} />
+                        <Stack.Screen name="Question5" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question5Screen} />
+                        <Stack.Screen name="Question6" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question6Screen} />
+                        <Stack.Screen name="Question7" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question7Screen} />
+                        <Stack.Screen name="Question8" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question8Screen} />
+                        <Stack.Screen name="Question9" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question9Screen} />
+                        <Stack.Screen name="Question10" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question10Screen} />
+                        <Stack.Screen name="Question11" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question11Screen} />
+                        <Stack.Screen name="Question12" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={Question12Screen} />
+                        <Stack.Screen name="ResultatFormation" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={ResultatFormation} />
+                        <Stack.Screen name="ResultatProduits" options={{ headerShown: false, gestureEnabled: false, navigationBarHidden: true }} component={ResultatProduits} />
                     </Stack.Navigator>
                 </NavigationContainer>
             )
